f0c14eaa66ecc50b5769fd8aa96f24ac5a5cbabb
N-sangwa <nadiinesangwa@gmail.com>
2022-08-14 10:21:23 +0300

    Bill calculating app

---
+++ main.js
@@ -0,0 +1,31 @@
+
+const billTotalInputDiv = document.getElementById('billTotalInput')
+const tipInputDiv = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
+
+let totalPeople = Number(numberOfPeopleDiv.innerText)
+
+const calculateBill = () => {
+  let billAmaount = Number(billTotalInputDiv.value)  
+  let tipPercentage = Number(tipInputDiv.value)/100
+  let totalTipAmount =  billAmaount*tipPercentage
+  let wholeTotal =   billAmaount + totalTipAmount
+  let billPerPerson = wholeTotal/totalPeople
+  perPersonTotalDiv.innerText = `$ ${billPerPerson.toLocaleString('en-US')}`
+ }     
+const increasePeople = () => {
+   totalPeople +=1
+  numberOfPeopleDiv.innerText = totalPeople
+  
+  calculateBill()
+  }
+const decreasePeople = () => {
+  if(totalPeople <= 1 ){
+    return
+  }
+
+  totalPeople -= 1
+numberOfPeopleDiv.innerText = totalPeople
+  calculateBill()
+  }
